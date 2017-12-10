854fdbdf6e54b13da55c8a66ae29d302d4c88d5b
LuletterSoul <qq313700046@icloud.com>
2017-12-10 15:04:21 +0800

    易授·app后台服务项目。

---
+++ yishou-core/src/main/java/edu/vero/yishou/api/entities/WW.java
@@ -0,0 +1,11 @@
+package edu.vero.yishou.api.entities;
+
+/**
+ * @author XiangDe Liu qq313700046@icloud.com .
+ * @version 1.5
+ *          created in  15:04 2017/12/10.
+ * @since yishou
+ */
+
+public class WW {
+}
