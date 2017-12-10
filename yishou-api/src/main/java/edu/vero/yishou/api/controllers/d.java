4eb8beed21a1a6391359190b7e3f354cc673573f
LuletterSoul <qq313700046@icloud.com>
2017-12-10 14:33:41 +0800

    易授·app后台服务项目。

---
+++ yishou-api/src/main/java/edu/vero/yishou/api/controllers/d.java
@@ -0,0 +1,11 @@
+package edu.vero.yishou.api.controllers;
+
+/**
+ * @author XiangDe Liu qq313700046@icloud.com .
+ * @version 1.5
+ *          created in  2:43 2017/12/8.
+ * @since yishou
+ */
+
+public class d {
+}
